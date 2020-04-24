4bcd7ab4e2866ab9531b17e047092c87b5eaea7d
Terence Cole <trcexcel@yahoo.com>
2020-04-24 17:41:56 -0500

    Modal pops up larger images

--- click-outside.js
+++ click-outside.js
@@ -1,1 +1,23 @@
-console.log('it works!');
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  // Grab the image src
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  const name = card.querySelectorAll('h2').textContent;
+  // populate the modal with the new info
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+  // show the modal
+  modalOuter.classList.add('open');
+}
+
+cardButtons.forEach(button =>
+  button.addEventListener('click', handleCardButtonClick)
+);
