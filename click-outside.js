096f381476b29aebfe4828906abe5c6520a2cc04
Terence Cole <trcexcel@yahoo.com>
2020-04-24 19:08:46 -0500

    Finished the modal demo

--- click-outside.js
+++ click-outside.js
@@ -11,7 +11,10 @@ function handleCardButtonClick(event) {
   const name = card.querySelectorAll('h2').textContent;
   // populate the modal with the new info
   modalInner.innerHTML = `
-    <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+    <img width="600" height="600" src="${imgSrc.replace(
+      '200',
+      '600'
+    )}" alt="${name}"/>
     <p>${desc}</p>
   `;
   // show the modal
@@ -21,3 +24,20 @@ function handleCardButtonClick(event) {
 cardButtons.forEach(button =>
   button.addEventListener('click', handleCardButtonClick)
 );
+
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(event) {
+  const isOutside = !event.target.closest('.modal-inner');
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
